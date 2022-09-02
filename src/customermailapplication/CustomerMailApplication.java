d2549cf8e6882720f88ef5ca145ca590023aa904
GUN <63050151@kmitl.ac.th>
2022-09-02 10:01:26 +0700

    first commit

---
+++ src/customermailapplication/CustomerMailApplication.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package customermailapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sarun
+ */
+public class CustomerMailApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    private Customer customer;
+    public void getCustomerTypeFromUser(String customerType) {        
+        switch(customerType) {
+            case "Regular":
+                customer = new RegularCustomer();
+                break;
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
+        }
+    }
+    public static void main(String[] arg) {
+        Customer cust = getCustomerTypeFromUser();
+        String mail = null;
+        if (cust != null) {
+            mail = generateMail(cust);
+            System.out.println(mail);
+        } else {
+            System.out.print("ERROR");
+        }
+    }
+    public static String generateMail(Customer cust) {
+        return cust.createMail();
+    }
+
+    public static Customer getCustomerTypeFromUser() {
+        Customer cust = null;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
+        int type = inp.nextInt();
+        switch(type) {
+            case 1:
+                cust = CustomerFactory.createCustomer("Regular");
+                break;
+            case 2:
+                 cust = CustomerFactory.createCustomer("Mountain");
+                break;
+            case 3:
+                cust = CustomerFactory.createCustomer("Delinquent");
+                break;
+            
+        }
+       return cust;        
+    }
+}
